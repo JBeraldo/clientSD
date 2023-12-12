d1ccbf68fff572b39f99945825d5472acb8da509
João <joaoangelo409@gmail.com>
2023-12-12 00:57:09 -0300

    finalizado plus

--- src/main/java/com/sd/client/view/controllers/GenerateRouteController.java
+++ src/main/java/com/sd/client/view/controllers/GenerateRouteController.java
@@ -4,6 +4,7 @@ import com.sd.client.app.models.Point;
 import com.sd.client.app.models.Segment;
 import com.sd.client.app.repositories.PointRepository;
 import com.sd.client.app.repositories.SegmentRepository;
+import com.sd.client.view.Client;
 import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
@@ -64,5 +65,9 @@ public class GenerateRouteController extends BaseController implements Initializ
         distance_cell.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(Integer.toString(cellData.getValue().getDistance())));
         direction_cell.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getDirection()));
         observation_cell.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getObservation()));
+    }
+
+    public void back(ActionEvent actionEvent) {
+        Client.changeScreen(getStage(route_tbl),"menu/menu_user.fxml");
     }
 }
